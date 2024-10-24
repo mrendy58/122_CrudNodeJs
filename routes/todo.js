78af9001aa1d1105138a5b9658860761c3413f9b
mrendy58 <derendyyy58@gmail.com>
2024-10-24 14:17:38 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -11,4 +11,36 @@ router.get('/', (req, res) => {
     res.json(todos); // Kirim todos sebagai respon JSON
 });
 
+router.post('/',(req,res)=>{
+    const newTodo = {
+        id: todos.length + 1,
+        task: req.body.task,
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+//Endpoint untuk menghapus tugas
+router.delete('/:id', (req, res) => {
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) return res.status(404).json({ message: 'Tugas Tidak Ditemukan' });
+
+    const deletedTodo = todos.splice(todoIndex, 1)[0]; // Menghapus dan menyimpan todo yang dihapus
+    res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
+});
+
+//enedpoint untuk memperbarui tugas
+router.put('/:id', (req,res)=>{
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json({message: 'Tugas Tidak Ditemukan'});
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: 'Tugas Dengan ID ${todo.id} telah diperbarui',
+        updatedTod: todo
+    });
+});
+
+
 module.exports = router; // Ekspor objek router
