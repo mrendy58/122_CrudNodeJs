950c0b6221615967f2866d4625b3b9300fa8491c
mrendy58 <derendyyy58@gmail.com>
2024-12-04 23:38:57 +0700

    STYLING_PAWC_122

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const todoRoutes = require("./routes/tododb.js"); // Disesuaikan untuk memastika
 const app = express();
 require("dotenv").config();
 //pertemuan 7
-const bcrypt = require('bcryptjs')
-const session = require('express-session');
-const authRoutes = require('./routes/authRoutes.js');
-const {isAuthenticated} = require('./middlewares/middleware.js');
-
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const authRoutes = require("./routes/authRoutes.js");
+const { isAuthenticated } = require("./middlewares/middleware.js");
 
 const port = process.env.PORT;
 
@@ -19,6 +18,8 @@ app.use(expressLayout);
 
 app.use(express.json());
 
+app.use(express.static("public"));
+
 // Routes untuk /todos akan ditangani oleh todo.js
 app.use("/todos", todoRoutes);
 
@@ -29,30 +30,32 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 // Konfigurasi express-session
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
-}));
+    cookie: { secure: false }, // Set ke true jika menggunakan HTTPS
+  })
+);
 
-app.use('/',authRoutes);
+app.use("/", authRoutes);
 
 // Route untuk halaman utama
-app.get("/", isAuthenticated,(req, res) => {
+app.get("/", isAuthenticated, (req, res) => {
   res.render("index", {
     layout: "layouts/main-layout",
   }); // Render view index.ejs
 });
 
 // Route untuk halaman kontak
-app.get("/contact",isAuthenticated, (req, res) => {
+app.get("/contact", isAuthenticated, (req, res) => {
   res.render("contact", {
     layout: "layouts/main-layout",
   }); // Render view contact.ejs
 });
 
-app.get("/todo-view",isAuthenticated, (req, res) => {
+app.get("/todo-view", isAuthenticated, (req, res) => {
   db.query("SELECT * FROM todos", (err, todos) => {
     if (err) return res.status(500).send("Internal Server Error");
     res.render("todo", {
